2285f2754e0376cae5ba07563ed197c486aa793d
leagrigat <learigat@gmail.com>
2024-01-05 14:10:19 +0100

    adjusted pages and layout

--- src/layouts/LayoutWithoutNav.tsx
+++ src/layouts/LayoutWithoutNav.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 function LayoutWithoutNav() {
-  return <div></div>;
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
 }
 
 export default LayoutWithoutNav;
--- src/layouts/NavbarLayout.tsx
+++ src/layouts/NavbarLayout.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 function NavbarLayout() {
-  return <div></div>;
+  return (
+    <div>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <nav></nav>
+      </footer>
+    </div>
+  );
 }
 
 export default NavbarLayout;
