9c71b93c293a4863e0838539f016cd0e0b6d670f
ohno <TheDrop@gmx.net>
2024-01-05 16:13:43 +0100

    created GenreIcons component

---
+++ src/components/GenreIcons.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function GenreIcons() {
+  return <div></div>;
+}
+
+export default GenreIcons;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../components/Button';
 
 function LoginPage() {
   return <div></div>;
