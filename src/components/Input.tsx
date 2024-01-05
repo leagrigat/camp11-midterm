31278920ac73eeca4625088f962721769565a21b
Staff <bpeters@posteo.de>
2024-01-05 16:01:27 +0100

    input component for review

---
+++ src/components/Input.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { cn } from '../utils/cn';
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+interface Props extends InputProps {
+  id: string;
+  label: string;
+  icon: React.ReactElement;
+  isRounded?: boolean;
+}
+
+function Input({ id, label, isRounded, icon, placeholder, ...props }: Props) {
+  return (
+    <div className={cn("gap-2 mb-5 mx-5 py-3 px-4 rounded-lg bg-white-heavy", isRounded && "rounded-full")}>
+      <label htmlFor={id} className="flex items-center gap-5">
+        <div className="text-white-dimmed py-3 pl-5">{icon}</div>
+        <input
+          name="email"
+          type="email"
+          placeholder={placeholder}
+          id={id}
+          {...props}
+          onChange={() => {}}
+          className="bg-white-heavy placeholder-white-dimmed"/>
+      </label>
+    </div>
+  );
+}
+
+export default Input;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
+import Input from './components/Input';
+import { FaRegUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa6";
+import { IoSearch } from "react-icons/io5";
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="bg-slate-500">Hello Camp#11 Midterm Project</div>
+      <Input icon={<FaRegUser />} id='email' label='email' placeholder='"your@email.com"'/>
+      <Input icon={<FaLock />} id='password' label='password' placeholder='Enter your Password'/>
+      <Input icon={<IoSearch />} id='search' label='search' placeholder='Search' isRounded={true} />
   </React.StrictMode>
 );
