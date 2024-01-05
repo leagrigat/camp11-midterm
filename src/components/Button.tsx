d26666f1b91fad2d79c23e10a83dee82e12fd213
ohno <TheDrop@gmx.net>
2024-01-05 12:21:59 +0100

    color changes

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Definiere die ButtonProps
+interface ButtonProps {
+  onClick: () => void;
+  label: string;
+}
+
+// Definiere die Button-Komponente
+const Button: React.FC<ButtonProps> = ({ onClick, label }) => {
+  return (
+    <button className="bg-primary" onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
