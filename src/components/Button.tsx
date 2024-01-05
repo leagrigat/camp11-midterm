332e51ef05c26037be800a499836e53bfe353db2
ohno <TheDrop@gmx.net>
2024-01-05 13:55:41 +0100

    add button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import { cn } from '../utils/cn';
 
-// Definiere die ButtonProps
-interface ButtonProps {
-  onClick: () => void;
-  label: string;
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+interface Props extends ButtonProps {
+  variant?: 'primary' | 'secondary';
+  size?: 'sm' | 'md' | 'lg';
 }
 
-// Definiere die Button-Komponente
-const Button: React.FC<ButtonProps> = ({ onClick, label }) => {
+function Button({
+  children,
+  variant = 'primary',
+  size = 'lg',
+  ...props
+}: Props) {
   return (
-    <button className="bg-secondary" onClick={onClick}>
-      {label}
+    <button
+      {...props}
+      className={cn(
+        'text-white  rounded-md',
+        variant === 'primary' && 'bg-primary',
+        variant === 'secondary' && 'bg-secondary',
+        size === 'sm' && 'text-sm py-2 px-4',
+        size === 'md' &&
+          'text-sm py-2 px-6 text-center w-[335px] h-[38px] rounded-[8px]',
+        size === 'lg' &&
+          'text-sm py-2 px-6 text-center w-[335px] h-[49px] rounded-[8px]'
+      )}
+    >
+      {children}
     </button>
   );
-};
+}
 
 export default Button;
