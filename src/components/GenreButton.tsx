d2b11f9fdc1f58034b7f472ef586a8a9ee7fae98
Rayk Oscuro <wer.auch.immer@gmx.net>
2024-01-08 11:26:47 +0100

    Genre text added, sizing and positioning fixed.

--- src/components/GenreButton.tsx
+++ src/components/GenreButton.tsx
@@ -4,28 +4,46 @@ import { genresLibary } from '../context/GenreProvider';
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 interface Props extends ButtonProps {
-  genreId?: Number|null;
-  genre?: String|null;
+  genreId?: number | null;
+  genre?: string | null;
 }
 
-function filterById(gId:Number){
-  return genresLibary.filter(obj=>{return obj.id === gId})[0]
+function filterById(gId: number) {
+  return genresLibary.filter(obj => {
+    return obj.id === gId;
+  })[0];
 }
 
-function filterByGenre(genre:String){
-  return genresLibary.filter(obj=>{return obj.genre === genre})[0]
+function filterByGenre(genre: String) {
+  return genresLibary.filter(obj => {
+    return obj.genre === genre;
+  })[0];
 }
 
-function GenreButton({ children, genreId = null, genre=null, ...props }: Props) {
+function GenreButton({
+  children,
+  genreId = null,
+  genre = null,
+  ...props
+}: Props) {
+  if (!genreId && genre) {
+    genreId = filterByGenre(genre).id;
+  }
+  if (genreId && !genre) {
+    genre = filterById(genreId).genre;
+  }
+  const emoji = filterById(genreId!).emoji;
   return (
-    <button
-      {...props}
-      className="bg-white-dimmed disabled:bg-dark-light text-center rounded-[0.75rem] patext-center text-3xl p-[0.81rem]"
-    >
-      {genreId && filterById(genreId).emoji}
-      {!genreId && genre && filterByGenre(genre).emoji}
-      {children}
-    </button>
+    <div className="flex flex-col items-center gap-2 w-fit">
+      <button
+        {...props}
+        className="bg-white-dimmed disabled:bg-dark-light text-center rounded-[0.75rem] text-center text-3xl w-14 h-14"
+      >
+        <p className="w-full text-center">{emoji}</p>
+        {children}
+      </button>
+      <h5 className="text-xs font-bold text-white-dimmed">{genre}</h5>
+    </div>
   );
 }
 
