58b59e8b2bd92289144d5b5b9989ee506464d0fa
Rayk Oscuro <wer.auch.immer@gmx.net>
2024-01-08 13:05:49 +0100

    Added BoringGenreButton.tsx with simpler functionality.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,18 @@
+import BoringGenreButton from '../components/BoringGenreButton';
 import GenreButton from '../components/GenreButton';
 
 function LoginPage() {
-  return <div><GenreButton genre={"Adventure"}></GenreButton><GenreButton genreId={28}></GenreButton></div>;
+  return (
+    <div>
+      <GenreButton genre={'Adventure'}></GenreButton>
+      <GenreButton genreId={28}></GenreButton>
+      <BoringGenreButton
+        genre={'Adventure'}
+        genreIcon={'💎'}
+        active={true}
+      ></BoringGenreButton>
+    </div>
+  );
 }
 
 export default LoginPage;
