9f84a849bae6bd1c17b4deccf8fb9868e69376f1
Staff <bpeters@posteo.de>
2024-01-08 09:15:40 +0100

    input email delete

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -10,14 +10,12 @@ interface Props extends InputProps {
   isRounded?: boolean;
 }
 
-function Input({ id, label, isRounded, icon, placeholder, ...props }: Props) {
+function Input({ id, label, isRounded, icon, ...props }: Props) {
   return (
     <div className={cn("gap-2 mb-5 mx-5 py-3 px-4 rounded-lg bg-white-heavy", isRounded && "rounded-full")}>
       <label htmlFor={id} className="flex items-center gap-5">
         <div className="text-white-dimmed py-3 pl-5">{icon}</div>
         <input
-          name="email"
-          type="email"
           placeholder={placeholder}
           id={id}
           {...props}
