f8ace64cd8bc10d3207931221e9a58df733e578c
Student <alebarriasanchez@gmail.com>
2024-01-08 12:02:04 +0100

    pages header

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
+import { Link, useNavigate } from 'react-router-dom';
+
+type HeaderProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+interface Props extends HeaderProps {
+  header: string;
+  icon?: React.ReactElement;
+}
+
+function Header({ header, icon }: Props) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className="flex items-center font-bold px-5 text-white justify-between">
+      <Link to="#" onClick={handleClick}>
+        <IoIosArrowBack />
+      </Link>
+
+      <h2>{header}</h2>
+      <div>{icon}</div>
+    </div>
+  );
+}
+
+export default Header;
