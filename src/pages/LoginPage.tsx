dc828a1f8c107043da12ef535013b68b731c1939
Rayk Oscuro <wer.auch.immer@gmx.net>
2024-01-09 10:10:15 +0100

    working in LoginPage.tsx for now (for instant testing)

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Header from '../components/Header';
+import Button from '../components/Button';
 
 function LoginPage() {
-  return <div></div>;
+  return <div className='flex flex-col bg-dark px-5 pt-8 h-full'><Header header={'Select Date & Time'}></Header><Button></Button></div>;
 }
 
 export default LoginPage;
---
+++ src/pages/SelectTimePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SelectTimePage() {
+  return <div></div>;
+}
+
+export default SelectTimePage;
