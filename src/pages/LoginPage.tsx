111d72ced7cb8dcfc738977d7cebae577ea17866
Rayk Oscuro <wer.auch.immer@gmx.net>
2024-01-09 10:58:40 +0100

    added some layout stuff, but need components from new main now...

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import Button from '../components/Button';
 
 function LoginPage() {
-  return <div className='flex flex-col bg-dark px-5 pt-8 h-full'><Header header={'Select Date & Time'}></Header><Button></Button></div>;
+  return <div className='flex flex-col bg-dark px-5 py-8 h-full place-content-between'><Header header={'Select Date & Time'}></Header><span className='text-white-dimmed font-bold text-sm'>DATE</span><hr className='h-px bg-white-heavy border-0'></hr><span className='text-white-dimmed font-bold text-sm'>TIME</span><Button>Select Seat</Button></div>;
 }
 
 export default LoginPage;
