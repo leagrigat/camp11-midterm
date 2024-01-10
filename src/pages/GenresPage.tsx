7a2dd1a5091ac10077f80670339ee9c090d80bbf
Devhaus Academy <gamzelino@hotmail.de>
2024-01-10 12:08:27 +0100

    finished genres page

--- src/pages/GenresPage.tsx
+++ src/pages/GenresPage.tsx
@@ -8,24 +8,36 @@ import { useNavigate } from 'react-router';
 function GenresPage() {
   const { genres, updateGenre, selectedCount } = useContext(GenreContext);
   const navigate = useNavigate();
+
   return (
-    <div className="flex flex-wrap gap-[37px] mx-5">
-      {genres.map(g => (
-        <GenreButton
-          onClick={() => {
-            updateGenre(g.id);
-          }}
-          key={g.id}
-          genreIcon={g.emoji}
-          active={g.isSelected}
-          genre={g.genre}
-        />
-      ))}
-      <div className="flex font-semibold gap-1">
-        <span className="flex text-white">{selectedCount}</span>
-        <p className="flex text-white-dimmed">Genres selected</p>
+    <div className="px-5 py-[16px] h-full flex flex-col justify-between">
+      <div>
+        <Header header="Genres"></Header>
+        <div className="flex flex-col justify-between">
+          <div className="flex flex-wrap gap-[37px] justify-center">
+            {genres.map(genre => (
+              <GenreButton
+                onClick={() => {
+                  updateGenre(genre.id);
+                }}
+                key={genre.id}
+                genreIcon={genre.emoji}
+                active={genre.isSelected}
+                genre={genre.genre}
+              />
+            ))}
+          </div>
+        </div>
       </div>
-      <Button onClick={() => navigate('/home')}>Confirm selected Genres</Button>
+      <div className="flex flex-col font-semibold gap-3">
+        <div className="flex gap-1">
+          <span className=" text-white">{selectedCount}</span>
+          <p className=" text-white-dimmed">Genres selected</p>
+        </div>
+        <Button onClick={() => navigate('/home')}>
+          Confirm selected Genres
+        </Button>
+      </div>
     </div>
   );
 }
