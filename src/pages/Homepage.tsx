dd258eb1cc76d75510ff6597d39b77c13be6512a
ohno <TheDrop@gmx.net>
2024-01-16 15:29:43 +0100

    slider gefixed

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -26,10 +26,8 @@ function Homepage() {
         </div>
         <GenreComponent />
         {/* h2 and images div have to be inside 1 container so that we can remove the gap between them */}
-        <h2 className="text-white text-base font-bold fixed bottom-[340px]">
-          Upcoming Movies
-        </h2>
-        <div className="overflow-scroll snap-x fixed bottom-[88px] left-0">
+        <h2 className="text-white text-base font-bold">Upcoming Movies</h2>
+        <div className="overflow-scroll snap-x -mx-5">
           <div className="flex gap-x-9 items-center">
             {isLoading ? (
               <LoadingSpinner />
