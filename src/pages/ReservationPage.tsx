b3c8fd1b8c9bfb94407fc0760f7e6e772cea76b5
Devhaus Academy <gamzelino@hotmail.de>
2024-01-16 16:24:49 +0100

    started

--- src/pages/ReservationPage.tsx
+++ src/pages/ReservationPage.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import Button from '../components/Button';
+import { useGetSingleMovie } from '../hooks/useGetSingleMovie';
+import { useGetMovies } from '../hooks/useGetMovies';
 
-function ReservationPage() {
-  return <div></div>;
+function Ticket() {
+  const { movieId } = useParams();
+
+  const { movies } = useGetMovies();
+
+  return (
+    <div className="">
+      <div className="bg-dark-light rounded-lg">
+        <img
+          key={movieId}
+          src={`https://image.tmdb.org/t/p/w300/${movieId!.backdrop_path}`}
+          alt=""
+        />
+      </div>
+      <Link to={`/home`}>
+        <Button size="lg" variant="primary">
+          Back to Home
+        </Button>
+      </Link>
+    </div>
+  );
 }
 
-export default ReservationPage;
+export default Ticket;
