da1d85ff4c3fc892156b7af98bb81baf6b9c213e
leagrigat <learigat@gmail.com>
2024-01-23 15:16:02 +0100

    did some formatting on MovieDataWrapper

--- src/components/MovieDataWrapper.tsx
+++ src/components/MovieDataWrapper.tsx
@@ -1,24 +1,23 @@
+import { useGetSingleMovie } from '../hooks/useGetSingleMovie';
 import SingleMoviePage from '../pages/SingleMoviePage';
 import NotFoundPage from '../pages/NotFoundPage';
-import { useGetSingleMovie } from '../hooks/useGetSingleMovie';
 import LoadingSpinner from './LoadingSpinner';
 
 function MovieDataWrapper() {
-  // fetch movie data
   const { movie, isLoading } = useGetSingleMovie();
-  return (
-    <div>
-      {isLoading ? (
-        <div className="h-screen top-0 flex flex-col justify-center items-center">
-          <LoadingSpinner />
-        </div>
-      ) : !movie || !movie.title ? (
-        <NotFoundPage />
-      ) : (
-        <SingleMoviePage />
-      )}
-    </div>
-  );
+
+  if (isLoading)
+    return (
+      <div className="h-screen top-0 flex flex-col justify-center items-center">
+        <LoadingSpinner />
+      </div>
+    );
+
+  if (!movie || !movie.title) {
+    return <NotFoundPage />;
+  }
+
+  return <SingleMoviePage />;
 }
 
 export default MovieDataWrapper;
