a0d68d97e060a3834adb21823cf768d32f049876
leagrigat <leagrigat@gmail.com>
2024-01-24 14:51:02 +0100

    app.post and console.logs in server.ts

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,14 @@ import 'dotenv/config';
 import cors from 'cors';
 //import { LogIn, Register } from './controllers';
 
+interface ReservationData {
+  movieId: number,
+  title: string,
+  date: string,
+  seats: string[],
+  totalPrice: number
+}
+
 //serverport
 const PORT = process.env.PORT;
 const app = express();
@@ -20,6 +28,20 @@ app.get('/', (req, res, next) => {
 app.get('/register', (req, res, next) => {
   res.json({ success: 'Yay we are best' });
 });
+
+// route reservation
+app.post('/reservation', (req, res, next) => {
+  const {movieId, title, date, seats, totalPrice}: ReservationData = req.body;
+
+  console.log("Movie ID", movieId);
+  console.log("Title", title);
+  console.log("Date", date);
+  console.log("Seats", seats);
+  console.log("Total Price", totalPrice);
+
+  res.status(201).json({message: "Reservation successful"});
+
+})
 
 app.listen(PORT, () => {
   console.log('port is running');
