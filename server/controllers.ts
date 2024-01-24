f2e7e41255ddc5efe730ead7e86ca2a96b977529
Devhaus Academy <gamzelino@hotmail.de>
2024-01-24 10:54:49 +0100

    set up express server and basic routes

---
+++ server/controllers.ts
@@ -0,0 +1,12 @@
+type LogIn = {
+  email: string;
+  password: string;
+};
+
+type Register = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  passwordRepeat: string;
+};
---
+++ server/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import { LogIn, Register } from './controllers';
+
+//serverport
+const PORT = process.env.PORT;
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//route logIn Page
+app.get('/', (req, res, next) => {
+  res.json({ success: 'Yay we are best' });
+});
+
+//route register Page
+app.get('/register', (req, res, next) => {
+  res.json({ success: 'Yay we are best' });
+});
+
+app.listen(PORT, () => {
+  console.log('port is running');
+});
