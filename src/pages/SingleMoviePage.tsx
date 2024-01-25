bd2c96382e2f449b4f4041324defd671023f7f72
Rayk Oscuro <wer.auch.immer@gmx.net>
2024-01-25 14:45:31 +0100

    final changes

--- src/pages/SingleMoviePage.tsx
+++ src/pages/SingleMoviePage.tsx
@@ -87,12 +87,10 @@ function SingleMoviePage() {
           header="Movie Detail"
           icon={
             <div onClick={switchFavData}>
-              {cn(
-                favorite ? (
-                  <FaHeart className="text-error" />
-                ) : (
-                  <FaRegHeart className="text-error" />
-                )
+              {favorite ? (
+                <FaHeart className="text-error" />
+              ) : (
+                <FaRegHeart className="text-error" />
               )}
             </div>
           }
@@ -150,7 +148,9 @@ function SingleMoviePage() {
       <div className="flex flex-col gap-2 items-start">
         <span className="text-white font-bold text-sm">Synopsis</span>
         <p
-          className={cn('text-white-dimmed text-sm', isOpen && 'line-clamp-2')}
+          className={cn('text-white-dimmed text-sm',
+          isOpen && 'line-clamp-2'
+        )}
         >
           {movie?.overview}
         </p>
