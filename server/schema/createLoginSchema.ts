eb408206184926fa2983e4246ab5d20dd5909c7a
Staff <bpeters@posteo.de>
2024-01-29 16:50:42 +0100

    create a zod schema for creating and login user

---
+++ server/schema/createLoginSchema.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+
+export const createUser = z.object({
+    firstName: z
+        .string()
+        .min(2, "First Name must be at least 2 characters.")
+        .max(15, "First Name can be maximum 15 characters."),
+    lastName: z
+        .string()
+        .min(2, "Last Name must be at least 2 characters.")
+        .max(15, "Last Name can be maximum 15 characters."),
+    email: z
+        .string()
+        .email("Invalid Email address. Should include @ Sign."),
+    password: z
+        .string()
+        .min(8, "Must be 8 or more characters long")
+        .refine(
+            value => /[A-Z]/.test(value),
+            'Password must contain at least one upper case letter!'
+          )
+          .refine(
+            value => /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(value),
+            'Password must contain at least one special character!'
+          )
+});
+
+
+export const loginUser = z.object({
+    email: z
+        .string()
+        .email("Invalid Email address. Should include @ Sign."),
+    password: z
+        .string()
+        .min(8, "Must be 8 or more characters long")
+        .refine(
+            value => /[A-Z]/.test(value),
+            'Password must contain at least one upper case letter!'
+          )
+          .refine(
+            value => /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(value),
+            'Password must contain at least one special character!'
+          )
+})
+
