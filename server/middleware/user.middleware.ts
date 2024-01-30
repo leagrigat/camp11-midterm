e62ad0400d5046d94bee846ad2e2aa587ba09826
Devhaus Academy <gamzelino@hotmail.de>
2024-01-30 12:15:03 +0100

    deleted some comments

--- server/middleware/user.middleware.ts
+++ server/middleware/user.middleware.ts
@@ -2,21 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 import { z } from 'zod';
 import { loginSchema, userSchema } from '../schema/createLoginSchema';
 
-//  export async function validate(
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) {
-//   try {
-//     loginSchema.parse(req.body);
-//     next();
-//   } catch (err) {
-//     return res.status(422).json(err);
-//   }
-// }
-
-//build type for schema
-
 export function validate<T extends z.ZodTypeAny>(schema: T) {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
