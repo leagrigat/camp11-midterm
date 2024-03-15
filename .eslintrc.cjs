e7cd4e868d307303049dcdcc8ca124fdfd9ea8a4
Bassem Allani <allani.bassem@yahoo.fr>
2024-03-15 21:23:32 +0100

    config: Configured Vitest & ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,19 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:vitest/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'vitest', 'testing-library'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
